a17107390682d94a2cb3497589b561b852d96eba
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-01 23:08:57 -0700

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -1,26 +1,25 @@
 from zksk import Secret, DLRep
 from zksk import utils
 
+
 def ZK_equality(G,H):
 
-
-  
-
-
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
-
-    r1 = random.randrange(2,p)
-    c1 = pow(g, r1, p)
-    d1 = (m * pow(pk, r, p)) % p
-
-    r2 = random.randrange(2,p)
-    c2 = pow(G, r2, p)
-    d2 = (m * pow(pk, r, p)) % p
     
+    r1 = Secret(utils.get_random_num(bits=128))
+    r1 = Secret(utils.get_random_num(bits=128))
     
-
+    m = Secret()
+    
+    C1 = r1 * G
+    C2 = r1 * H + m * G
+    D1 = r2 * G
+    D2 = r2 * H + m * G
+    
     #Generate a NIZK proving equality of the plaintexts
+    
+    stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
+    zk_proof = stmt.prove()
 
     #Return two ciphertexts and the proof
     return (C1,C2), (D1,D2), zk_proof
-
