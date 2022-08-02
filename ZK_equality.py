a75d3aa8649931cb5ef4cb720676b977b081ca09
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-02 00:51:45 -0700

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -11,10 +11,10 @@ def ZK_equality(G,H):
     
     m = Secret()
         
-    C1 = r1 * G
-    C2 = r1 * H + m * G
-    D1 = r2 * G
-    D2 = r2 * H + m * G
+    C1 = r1.value * G
+    C2 = r1.value * H + m * G
+    D1 = r2.value * G
+    D2 = r2.value * H + m * G
     
     #Generate a NIZK proving equality of the plaintexts
     
