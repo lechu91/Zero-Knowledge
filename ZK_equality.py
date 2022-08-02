4494b78eeca2bddbc0463db3f3f51e5065ccb02a
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-01 23:50:35 -0700

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -8,15 +8,15 @@ def ZK_equality(G,H):
     r1 = Secret(utils.get_random_num(bits=128))
     r2 = Secret(utils.get_random_num(bits=128))
     
-    m = 1
+    m = Secret()
     
     print(r1)
     print(r2)
         
-    C1 = r1 * G
-    C2 = r1 * H + m * G
-    D1 = r2 * G
-    D2 = r2 * H + m * G
+    C1 = r1.value * G
+    C2 = r1.value * H + m * G
+    D1 = r2.value * G
+    D2 = r2.value * H + m * G
     
     print(C1)
     print(C2)
