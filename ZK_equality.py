4e0955dc5a2004688d6ab0b7d82e9fb5ed07b177
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-08-01 23:38:53 -0700

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -5,18 +5,18 @@ def ZK_equality(G,H):
 
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
     
-    r1 = Secret(utils.get_random_num(bits=128))
-    r2 = Secret(utils.get_random_num(bits=128))
+    r1 = Secret(utils.get_random_num(bits=128)).value
+    r2 = Secret(utils.get_random_num(bits=128)).value
     
     m = 1
     
     print(G)
     print(H)
     
-    C1 = r1.value * G
-    C2 = r1.value * H + m * G
-    D1 = r2.value * G
-    D2 = r2.value * H + m * G
+    C1 = r1 * G
+    C2 = r1 * H + m * G
+    D1 = r2 * G
+    D2 = r2 * H + m * G
     
     #Generate a NIZK proving equality of the plaintexts
     
